903addf68c832eb2173e2436cb088adffe8b9a33
andrej5elin <andrej.petelin@ijs.si>
2018-04-23 23:58:26 +0200

    doc

--- examples/color_bandpass_filter.py
+++ examples/color_bandpass_filter.py
@@ -41,6 +41,7 @@ for k,delta in enumerate((50,150,400)):
     ax.set_yticklabels(intensities[::20])
     ax.set_ylabel("intensity [arb.]")
     
+    
 ax.set_xticks(range(nw)[::20])    
 ax.set_xticklabels(ws[::20])
 ax.set_xlabel("Band-pass filter central wavelength [nm]")
