aebc6e70c9f5bdd7ea4f9c1df654cc1a4c6b3d25
andrej5elin <andrej.petelin@ijs.si>
2018-11-23 22:24:43 +0100

    Update diffract.py

--- dtmm/diffract.py
+++ dtmm/diffract.py
@@ -26,22 +26,9 @@ def diffraction_alphaf(shape, ks, epsv = (1.,1.,1.),
     epsv = np.asarray(epsv, CDTYPE)
     epsa = np.asarray(epsa, FDTYPE)
 
-
     alpha, f= alphaf(beta,phi,epsv,epsa,out = out) 
 
     out = (alpha,f)
-    
-#    try:
-#        b1,b2 = betamax
-#        a = (b2-b1)/(b2)
-#        m = tukey(beta,a,b2)
-#        
-#        print(b1)
-#        m = (b1-beta.clip(b1,betamax))/(betamax-b1)+1
-#        
-#        np.multiply(f,m[...,None,None],f)
-#    except:
-#        pass
 
     mask0 = (beta >= betamax)
     
@@ -51,22 +38,6 @@ def diffraction_alphaf(shape, ks, epsv = (1.,1.,1.),
     return out
 
 
-#@cached_function
-#def diffraction_polarizer(shape, ks, jones = (1,0), epsv = (1.,1.,1.), 
-#                            epsa = (0.,0.,0.), betamax = BETAMAX, out = None):
-#    ks = np.asarray(ks)
-#    ks = abs(ks)
-#    epsv = np.asarray(epsv, CDTYPE)
-#    epsa = np.asarray(epsa, FDTYPE)
-#    beta, phi = betaphi(shape,ks)
-#    alpha, f = diffraction_alphaf(shape, ks, epsv = epsv, 
-#                            epsa = epsa, betamax = betamax)
-#
-#    beta, phi = betaphi(shape,ks)
-#    jones = jonesvec(jones, phi)
-#    pmat = polarizer4x4(jones, f)
-#    return pmat
-
 
 @cached_function
 def diffraction_alphaffi(shape, ks, epsv = (1.,1.,1.), 
@@ -75,34 +46,19 @@ def diffraction_alphaffi(shape, ks, epsv = (1.,1.,1.),
 
     ks = np.asarray(ks)
     ks = abs(ks)
-    #shape = fieldv.shape[-2:]
-    #eps = uniaxial_order(0.,eps0)
     beta, phi = betaphi(shape,ks)
-    #m = tukey(beta,0.3,betamax)
-    #mask0 = (beta>0.9) & (beta < 1.1)
-    
 
-    
-    #mask = np.empty(mask0.shape + (4,), mask0.dtype)
-    #for i in range(4):
-    #    mask[...,i] = mask0   
-
-     
     alpha, f, fi = alphaffi(beta,phi,epsv,epsa,out = out) 
 
     out = (alpha,f,fi)
     
-    try:
-        b1,betamax = betamax
-        a = (betamax-b1)/(betamax)
-        m = tukey(beta,a,betamax)
-        #print(b1)
-        #m = (b1-beta.clip(b1,betamax))/(betamax-b1)+1
-        #np.multiply(alpha,m[...,None,None],alpha)
-        np.multiply(f,m[...,None,None],f)
-        #np.multiply(fi,m[...,None,None],fi)
-    except:
-        pass
+#    try:
+#        b1,betamax = betamax
+#        a = (betamax-b1)/(betamax)
+#        m = tukey(beta,a,betamax)
+#        np.multiply(f,m[...,None,None],f)
+#    except:
+#        pass
     mask0 = (beta >= betamax)#betamax)
     fi[mask0] = 0.
     f[mask0] = 0.
@@ -118,27 +74,15 @@ def E_diffraction_alphaEEi(shape, ks, epsv = (1,1,1),
 
     ks = np.asarray(ks)
     ks = abs(ks)
-    #shape = fieldv.shape[-2:]
-    #eps = uniaxial_order(0.,eps0)
     beta, phi = betaphi(shape,ks)
-    #m = tukey(beta,0.,betamax)
-    #mask0 = (beta>0.9) & (beta < 1.1)
-    mask0 = (beta >= betamax)#betamax)
-    #mask = np.empty(mask0.shape + (4,), mask0.dtype)
-    #for i in range(4):
-    #    mask[...,i] = mask0   
 
+    mask0 = (beta >= betamax)#betamax)
             
     alpha, j, ji = alphaEEi(beta,phi,epsv,epsa, mode = mode, out = out) 
     ji[mask0] = 0.
     j[mask0] = 0.
     alpha[mask0] = 0.
     out = (alpha,j,ji)
-
-    #np.multiply(f,m[...,None,None],f)
-    #np.multiply(fi,m[...,None,None],fi)
-    #return mask, alpha,f,fi
-    
     return out
 
   
@@ -239,15 +183,15 @@ def diffracted_field(field, wavenumbers, d = 0.,n = 1, mode = "t", betamax = BET
     eps = refind2eps([n]*3)
     pmat = field_diffraction_matrix(field.shape[-2:], wavenumbers, d = d, epsv = eps, epsa = (0.,0.,0.), mode = mode, betamax = betamax)
     return diffract(field, pmat, out = out) 
-
-def transmitted_field(field, wavenumbers, n = 1, betamax = BETAMAX, out = None):
-    eps = refind2eps([n]*3)
-    pmat = projection_matrix(field.shape[-2:], wavenumbers, epsv = eps, epsa = (0.,0.,0.), mode = "t", betamax = betamax)
-    return diffract(field, pmat, out = out) 
-
-def reflected_field(field, wavenumbers, n = 1, betamax = BETAMAX, out = None):
-    eps = refind2eps([n]*3)
-    pmat = projection_matrix(field.shape[-2:], wavenumbers, epsv = eps, epsa = (0.,0.,0.), mode = "r", betamax = betamax)
-    return diffract(field, pmat, out = out) 
+#
+#def transmitted_field(field, wavenumbers, n = 1, betamax = BETAMAX, out = None):
+#    eps = refind2eps([n]*3)
+#    pmat = projection_matrix(field.shape[-2:], wavenumbers, epsv = eps, epsa = (0.,0.,0.), mode = "t", betamax = betamax)
+#    return diffract(field, pmat, out = out) 
+#
+#def reflected_field(field, wavenumbers, n = 1, betamax = BETAMAX, out = None):
+#    eps = refind2eps([n]*3)
+#    pmat = projection_matrix(field.shape[-2:], wavenumbers, epsv = eps, epsa = (0.,0.,0.), mode = "r", betamax = betamax)
+#    return diffract(field, pmat, out = out) 
 
 __all__ = []
