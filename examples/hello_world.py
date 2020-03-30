89ed4784e059cd6c4ca1efc9936c7eb54d387055
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2020-03-30 12:26:35 -0400

    Documented hello_world.py
    Change matplotlib backend in hello_world.py to an interactive one

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,28 +1,36 @@
-"""Hello nematic droplet example."""
+"""
+Hello nematic droplet example.
+"""
+# Change matplotlib backend to an interactive one
+import matplotlib
+matplotlib.use("TkAgg")
 
 import dtmm
 import numpy as np
 
-#: pixel size in nm
+#: Pixel size in nm
 PIXELSIZE = 200
-#: compute box dimensions
+#: Set box dimensions
 NLAYERS, HEIGHT, WIDTH = 60, 96, 96
-#: illumination wavelengths in nm
-WAVELENGTHS = np.linspace(380,780,9)
+#: Illumination wavelengths in nm
+WAVELENGTHS = np.linspace(380, 780, 9)
 #: create some experimental data (stack)
-optical_data = dtmm.nematic_droplet_data((NLAYERS, HEIGHT, WIDTH), 
-          radius = 30, profile = "r", no = 1.5, ne = 1.6, nhost = 1.5)
-window = dtmm.aperture((96,96))
+optical_data = dtmm.nematic_droplet_data(shape=(NLAYERS, HEIGHT, WIDTH),
+                                         radius=30, profile="r",
+                                         no=1.5, ne=1.6, nhost=1.5)
+# Create aperture for illumination light
+window = dtmm.aperture((96, 96))
 
 #: create non-polarized input light
 field_data_in = dtmm.field.illumination_data((HEIGHT, WIDTH), WAVELENGTHS,
-                                            pixelsize = PIXELSIZE) 
+                                             pixelsize=PIXELSIZE, window=window)
 #: transfer input light through stack
 field_data_out = dtmm.transfer_field(field_data_in, optical_data)
 
 #: visualize output field
 viewer = dtmm.field_viewer(field_data_out)
-viewer.set_parameters(sample = 0, intensity = 2, 
-                polarizer = 0, focus = -14, analyzer = 90)
-fig,ax = viewer.plot()
-
+viewer.set_parameters(sample=0, intensity=2,
+                      polarizer=0, focus=-14, analyzer=90)
+fig, ax = viewer.plot()
+# Show figure
+fig.show()
