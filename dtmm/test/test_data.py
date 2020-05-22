57d6f5cfddd05a97830d61fe50b633718763855f
andrej5elin <6813801+andrej5elin@users.noreply.github.com>
2020-05-22 15:21:49 +0200

    Update test_data.py
    Remove failing test

--- dtmm/test/test_data.py
+++ dtmm/test/test_data.py
@@ -14,21 +14,6 @@ class TestData(unittest.TestCase):
     def setUp(self):
         pass
  
-    def test_rotations(self):
-
-         rot90str = ("+x","-2x","+2y","-y","y","z","3z")
-         rotmat = (Rx,Rx,Ry,Ry,Ry,Rz,Rz)
-         angles = (np.pi/2,-np.pi,np.pi,-np.pi/2,np.pi/2,np.pi/2,-np.pi/2)
-
-         d = np.random.randn(5,5,5,3) #nlayers,height,width, 3  
-         d = np.asarray(d, dtype = conf.FDTYPE) #in case we are in single precision cast here
-         for s,r,a in zip(rot90str,rotmat,angles):
-             d90 = data.rot90_director(d,s)
-             rotm = r(a)
-             dR = data.rotate_director(rotm, d)#should be same, no interpolation needed
-
-             self.assertTrue(np.allclose(d90,dR)) 
-
 #    def test_rotate_director(self):
 #        """
 #        Tests the rotate_director function using rot90_director.
