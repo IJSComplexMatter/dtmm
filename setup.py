d34233c00dfc82014f53d32b1cf1444ffdc87a81
andrej5elin <andrej.petelin@ijs.si>
2020-05-22 16:55:16 +0200

    update setup.py
    imports version from __init__.py

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
-#import distribute_setup
-#distribute_setup.use_setuptools()
-
-# Setuptools Must be installed from Canopy and not by ez_setup...
-
-#import ez_setup
-#ez_setup.use_setuptools() 
-
+    
 from setuptools import setup, find_packages
+from dtmm import __version__
+
+long_description = """
+dtmm is an electro-magnetic field transmission and reflection calculation engine and visualizer. It can be used for calculation of transmission or reflection properties of layered homogeneous or inhomogeneous materials, such as confined liquid-crystals with homogeneous or inhomogeneous director profile. DTMM stands for Diffractive Transfer Matrix Method and is an adapted Berreman 4x4 transfer matrix method and an adapted 2x2 extended Jones method.
+"""
 
 packages = find_packages()
 
 setup(name = 'dtmm',
-      version = "0.0.2.dev0",
+      version = __version__,
       description = 'Diffractive Transfer Matrix Method',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author = 'Andrej Petelin',
       author_email = 'andrej.petelin@gmail.com',
+      url="https://github.com/IJSComplexMatter/dtmm",
       packages = packages,
       #include_package_data=True
-    package_data={
+      package_data={
         # If any package contains *.dat, or *.ini include them:
-        '': ['*.dat',"*.ini"]}
+        '': ['*.dat',"*.ini"]},
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
       )
