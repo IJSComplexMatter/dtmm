fec8902a95b8d1de28f32c6d5cba79863c74a8b6
andrej5elin <andrej.petelin@ijs.si>
2020-11-07 21:47:38 +0100

    coverage

--- run_tests.sh
+++ run_tests.sh
@@ -8,4 +8,4 @@ xargs <.coverage_examples.txt -I filename coverage run -a  "filename"
 popd
 #we are adding to coverage results, so move these in root dir
 mv examples/.coverage .coverage
-coverage run -a -m pytest dtmm --doctest-modules
+#coverage run -a -m pytest dtmm --doctest-modules
