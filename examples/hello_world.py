53beed1e1e217dba89d3d9d7dc55239aa9c441b8
andrej5elin <6813801+andrej5elin@users.noreply.github.com>
2021-05-21 14:51:16 +0200

    Update hello_world.py

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -4,11 +4,10 @@ import dtmm
 import numpy as np
 import matplotlib.pyplot as plt
 dtmm.conf.set_verbose(2)
-dtmm.conf.set_numba_threads(1)
 #: pixel size in nm
 PIXELSIZE = 200
 #: compute box dimensions
-NLAYERS, HEIGHT, WIDTH = 60, 256, 256
+NLAYERS, HEIGHT, WIDTH = 60, 96,96
 #: illumination wavelengths in nm
 WAVELENGTHS = np.linspace(380,780,9)
 #: create some experimental data (stack)
